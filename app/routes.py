76cee6e52e042e4c1af20c5d1454c0a4c76977ed
Rogerio-IFSP <rogifsp@gmail.com>
2021-03-18 22:06:06 +0000

    hello-world2

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from app import app
-from flask import render_template 
+from flask import render_template, request
 
-
-#o @ chama um URL route
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    user = {'username': 'Feulo'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html" , title="Login")
