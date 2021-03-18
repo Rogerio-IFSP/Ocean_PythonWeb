4d58ceed748fed0619e10038c4556a7996def0b0
Rogerio-IFSP <rogifsp@gmail.com>
2021-03-18 19:08:00 +0000

    hello-world

--- microblog.py
+++ microblog.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-app = Flask("microblog")
-
-#@ é comando decoreter (è um operador do PYTHOM que fica em cima de uma função e faz 
-#alguma coisa com essa função)
-#o @ chama um URL route
-@app.route("/")
-def index():
-    return "Olá Mundo!"
-
-# para executar
-app.run()
+from app import app
